3fdd916d64d4776d70e204d2006d3cbac2557a25
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-02-18 17:14:39 -0600

    add middleware for json

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json);
+app.use(express.urlencoded({ extended: false }));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

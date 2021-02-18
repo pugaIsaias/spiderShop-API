ff9bf55092e116e6bc5a9bcb3b3fdcc2b6844b30
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-02-18 17:41:13 -0600

    add Courses list

---
+++ Courses.js
@@ -0,0 +1,18 @@
+const Courses = [
+  {
+    id: 0,
+    name: "taller 5 lenguajes del amor",
+    description: "taller sobre los 5 lenguajes del amor",
+    price: "Q25",
+    link: "/4255",
+  },
+  {
+    id: 1,
+    name: "curso de bartender",
+    description: "aprende a hacer 3 tipos de tragos diferentes",
+    price: "Q25",
+    link: "/3576",
+  },
+];
+
+module.exports = Courses;

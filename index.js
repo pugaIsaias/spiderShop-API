f430ad6e73479669eaad320d1c7eefb3ab41cb6c
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-02-18 18:25:28 -0600

    middleware body-parser

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(express.json);
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/courses", require("./routes/api/courses"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

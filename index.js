b8adfd09cd95f8e566034dfd3cb7b8896a77745a
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-02-18 21:06:03 -0600

    fix to "Access-Control-Allow-Origin"

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTION, GET, POST, DELETE, PATCH, PUT"
+  );
+  res.setHeader("Access-Control-Allow-Header", "*");
+  next();
+});
+
 app.use("/api/courses", require("./routes/api/courses"));
 
 const PORT = process.env.PORT || 5000;

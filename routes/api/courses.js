28c554deea1fde78b8d18030eb0c5938552b5329
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-02-25 22:35:12 -0600

    set status 200 to get response

--- routes/api/courses.js
+++ routes/api/courses.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const courses = require("../../Courses");
 
 //Get all courses
-router.get("/", (req, res) => res.json(courses));
+router.get("/", (req, res) => res.status(200).json(courses));
 
 //Get single course
 router.get("/:id", (req, res) => {
   const found = courses.some((course) => course.id === parseInt(req.params.id));
 
   found
-    ? res.json(
-        courses.filter((course) => course.id === parseInt(req.params.id))
-      )
+    ? res
+        .status(200)
+        .json(courses.filter((course) => course.id === parseInt(req.params.id)))
     : res
         .status(400)
         .json({ msg: `No member with the id of ${req.params.id}` });
